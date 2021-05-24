13c795f5b7c4ca52605283bd1f74a7afd9727ba0
Virginia Wu <virginiawu@virginias-air.myfiosgateway.com>
2021-05-23 22:17:47 -0400

    add register user function

---
+++ forms.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+from wtforms.validators import InputRequired, Email, Length
+
+
+class UserForm(FlaskForm):
+    username = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password", validators=[InputRequired()])
+    email = StringField("Email", validators=[InputRequired(), Email(), Length(8,50)])
+    first_name = StringField("First Name", validators=[InputRequired(),Length(1,30)])
+    last_name = StringField("Last Name", validators=[InputRequired(),Length(1,30)])
+
